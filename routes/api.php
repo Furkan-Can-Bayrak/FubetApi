b75993f3b8c68d81be0717a7fe9b50cd436ab04d
Furkan-Can-Bayrak <furkancanb957@gmail.com>
2025-04-05 19:04:40 +0300

    api.php route'lar eklendi

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group([
+    'prefix' => 'users',
+    'controller' => UserController::class
+], function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
