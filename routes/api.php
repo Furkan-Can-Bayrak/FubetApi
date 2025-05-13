c073debb8f1ea43ff8747533dbfa507d9c3067f8
Furkan-Can-Bayrak <furkancanb957@gmail.com>
2025-05-13 13:59:37 +0300

    logout route'una middleware konuldu

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
 
 Route::get('/user', function (Request $request) {
