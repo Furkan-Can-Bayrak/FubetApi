ebdf70336099ef0a14577766baf80d82464e0de9
Furkan-Can-Bayrak <furkancanb957@gmail.com>
2025-05-13 16:37:26 +0300

    guest eklendi

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
 
 
 Route::get('/user', function (Request $request) {
